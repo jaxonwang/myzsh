0564a65519b1a52923ccad7b0e5ef41dcb8f9ac3
Jiexing Wang <jxwang@amazon.co.jp>
2022-05-25 06:53:38 +0900

    update

--- darwin.zsh
+++ darwin.zsh
@@ -1,9 +1,14 @@
 # homebrew
-eval "$(/opt/homebrew/bin/brew shellenv)"
+
+if [[ -f /opt/homebrew/bin/brew ]]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [[ -f /usr/local/bin/brew ]]; then
+    eval "$(/usr/local/bin/brew shellenv)"
+fi
+
 
 # auto complete
 if type brew &>/dev/null
 then
   FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 fi
-
